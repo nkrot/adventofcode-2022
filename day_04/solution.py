6fa794982c18e141b3f34f23fabe54773870b7bc
Nikolai Krot <talpus@gmail.com>
2022-12-04 12:08:24 +0100

    refactor day 04: - simplify code - use `run_tests()` and `run_real()` from `aoc/utils.py`

--- day_04/solution.py
+++ day_04/solution.py
@@ -4,10 +4,9 @@
 #
 #
 
-import re
 import os
 import sys
-from typing import List, Tuple, Callable
+from typing import List
 
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 from aoc import utils
@@ -16,70 +15,39 @@ DAY = '04'
 DEBUG = int(os.environ.get('DEBUG', 0))
 
 
-def parse(line: str) -> List[Tuple[int, int]]:
-    m = re.match(r'(\d+)-(\d+),(\d+)-(\d+)$', line)
-    return [
-        (int(m[1]), int(m[2])),
-        (int(m[3]), int(m[4]))
-    ]
-
-
-def is_contained(ts) -> bool:
-    ta, tb = sorted(ts)
-    return ta[1] >= tb[1] or ta[0] == tb[0]
-
-
-def overlap(ts) -> bool:
-    ta, tb = sorted(ts)
-    return ta[0] <= tb[0] <= ta[1]
-
-
-def solve(lines: List[str], evaluate: Callable) -> int:
-    return sum(map(evaluate, map(parse, lines)))
+def parse(line: str) -> List[List[int]]:
+    """Parse a line of input into suitable data stricture:
+    [[start, end], [start, end]]
+    """
+    ints = list(map(int, line.replace(',', '-').split('-')))
+    return [ints[:2], ints[2:]]
 
 
 def solve_p1(lines: List[str]) -> int:
     """Solution to the 1st part of the challenge"""
-    return solve(lines, is_contained)
+    def is_contained(ts) -> bool:
+        ta, tb = sorted(ts)
+        return ta[1] >= tb[1] or ta[0] == tb[0]
+    return sum(map(is_contained, lines))
 
 
 def solve_p2(lines: List[str]) -> int:
     """Solution to the 2nd part of the challenge"""
-    return solve(lines, overlap)
+    def overlap(ts) -> bool:
+        ta, tb = sorted(ts)
+        return ta[0] <= tb[0] <= ta[1]
+    return sum(map(overlap, lines))
 
 
 tests = [
-    (utils.load_input('test.1.txt'), 2, 4),
+    (utils.load_input('test.1.txt', line_parser=parse), 2, 4),
+]
+
+reals = [
+    (utils.load_input(line_parser=parse), 550, 931)
 ]
 
 
-def run_tests():
-    print("--- Tests ---")
-
-    for tid, (inp, exp1, exp2) in enumerate(tests):
-        if exp1 is not None:
-            res1 = solve_p1(inp)
-            print(f"T.{tid}.p1:", res1 == exp1, exp1, res1)
-
-        if exp2 is not None:
-            res2 = solve_p2(inp)
-            print(f"T.{tid}.p2:", res2 == exp2, exp2, res2)
-
-
-def run_real():
-    lines = utils.load_input()
-
-    print(f"--- Day {DAY} p.1 ---")
-    exp1 = 550
-    res1 = solve_p1(lines)
-    print(exp1 == res1, exp1, res1)
-
-    print(f"--- Day {DAY} p.2 ---")
-    exp2 = 931
-    res2 = solve_p2(lines)
-    print(exp2 == res2, exp2, res2)
-
-
 if __name__ == '__main__':
-    run_tests()
-    run_real()
+    utils.run_tests(DAY, tests, solve_p1, solve_p2)
+    utils.run_real(DAY, reals, solve_p1, solve_p2)
