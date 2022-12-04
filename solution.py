0df5b1a481f3469ec655f8fdf8cb3e2e9df908e0
Nikolai Krot <talpus@gmail.com>
2022-12-04 12:14:53 +0100

    refactor template `solution.py` to use facilities from `aoc/utils.py`

--- solution.py
+++ solution.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python
 
 # # #
-# template script
-# 1) copy it under a day-related directory
-# 2) fill in TODOs
+#
 #
 
 import re
@@ -16,6 +14,13 @@ from aoc import utils
 
 DAY = 'DD'  # TODO
 DEBUG = int(os.environ.get('DEBUG', 0))
+
+
+def parse(line: str) -> str:
+    """Parse a line of input into suitable data stricture:
+    """
+    # TODO: implement or delete if no transformation is needed
+    return line
 
 
 def solve_p1(lines: List[str]) -> int:
@@ -31,38 +36,16 @@ def solve_p2(lines: List[str]) -> int:
 
 
 tests = [
-    # (utils.load_input('test.1.txt'), exp1, None),
+    # (utils.load_input('test.1.txt', line_parser=parse), exp1, None),
     # TODO
 ]
 
 
-def run_tests():
-    print("--- Tests ---")
-
-    for tid, (inp, exp1, exp2) in enumerate(tests):
-        if exp1 is not None:
-            res1 = solve_p1(inp)
-            print(f"T.{tid}.p1:", res1 == exp1, exp1, res1)
-
-        if exp2 is not None:
-            res2 = solve_p2(inp)
-            print(f"T.{tid}.p2:", res2 == exp2, exp2, res2)
-
-
-def run_real():
-    lines = utils.load_input()
-
-    print(f"--- Day {DAY} p.1 ---")
-    exp1 = -1
-    res1 = solve_p1(lines)
-    print(exp1 == res1, exp1, res1)
-
-    print(f"--- Day {DAY} p.2 ---")
-    exp2 = -1
-    res2 = solve_p2(lines)
-    print(exp2 == res2, exp2, res2)
+reals = [
+    # (utils.load_input(line_parser=parse), None, None)
+]
 
 
 if __name__ == '__main__':
-    run_tests()
-    # run_real()
+    utils.run_tests(DAY, tests, solve_p1, solve_p2)
+    # utils.run_real(DAY, reals, solve_p1, solve_p2)
